fa01aaae9964eec0159409fb28340209ced43f56
stechinfo <sergedjato255@gmail.com>
2025-12-17 13:32:06 +0000

    submit_form.py

---
+++ submit_form.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, redirect
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    phone = request.form['phone']
+    service = request.form['service']
+    message = request.form['message']
+
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="sergeuser",  
+        password="Serge2530@", 
+        database="stechinfodb"
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO messages (name, email, phone, service, message) VALUES (%s, %s, %s, %s, %s)",
+                   (name, email, phone, service, message))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return redirect('/merci.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
