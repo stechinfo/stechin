39a3c2180bc1fc1fe305fd7840db79ac88ba3098
stechinfo <sergedjato255@gmail.com>
2025-12-18 06:22:15 +0000

    submit_form.py

--- submit_form.py
+++ submit_form.py
@@ -1,7 +1,11 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import mysql.connector
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -15,27 +19,28 @@ def submit():
         conn = mysql.connector.connect(
             host="localhost",
             user="sergeuser",
-            password="Serge2530",
+            password="Serge2530@",
             database="stechinfodb"
         )
 
         cursor = conn.cursor()
-
-        sql = """
-        INSERT INTO contacts (name, email, phone, service, message)
-        VALUES (%s, %s, %s, %s, %s)
-        """
-        cursor.execute(sql, (name, email, phone, service, message))
+        cursor.execute(
+            "INSERT INTO contacts (name, email, phone, service, message) VALUES (%s,%s,%s,%s,%s)",
+            (name, email, phone, service, message)
+        )
 
         conn.commit()
         cursor.close()
         conn.close()
 
-        return redirect("/Message envoyé avec succès")
+        return redirect('/success')
 
     except Exception as e:
-        return f"Erreur : {str(e)}", 500
+        return f"Erreur serveur : {str(e)}", 500
 
+@app.route('/success')
+def success():
+    return "Message envoyé avec succès ✅"
 
 if __name__ == '__main__':
     app.run(debug=True)
